46a162e713eae94abe3ebe6e8be9ea4a3174cd7a
610yilingliu <yilingliu1994@gmail.com>
2021-02-13 01:44:34 +0800

    count distence

--- tools/is_in.py
+++ tools/is_in.py
@@ -6,24 +6,8 @@ import pandas as pd
 from math import sqrt
 from collections import defaultdict
 
+from pandas.core.indexes import multi
 
-
-class Logger(object):
-    """
-    Logging module
-    """
-    def __init__(self, filename, stream=sys.stdout):
-	    self.terminal = stream
-	    self.log = open(filename, "wb", buffering=0)
-
-    def write(self, *message):
-        message = ",".join([str(it) for it in message])
-        self.terminal.write(str(message))
-        prefix = '[' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ']'
-        self.log.write(prefix.encode('utf-8') + str(message).encode('utf-8'))
-
-    def flush(self):
-        pass
 
 def time_helper(seperator = '_', to_sec = True):
     """
@@ -54,16 +38,7 @@ def is_inside(point, region):
     x1, y1, x2, y2, x3, y3, x4, y4 = region
     return cross(x1, y1, x2, y2, x, y) * cross(x3, y3, x4, y4, x, y) >= 0 and cross(x2, y2, x3, y3, x, y) * cross(x4, y4, x1, y1, x, y) >= 0
 
-
-if not os.path.exists('./log'):
-    os.mkdir('./log')
-start_time = time_helper()
-sys.stdout = Logger('./log/' + start_time + '.log')
-
-
-
-
-def output_func(line_per_proc, proc_id, bikes, fences):
+def output_func(line_per_proc, proc_id, bikes, fences, proc_num):
     l = line_per_proc if proc_id < proc_num - 1 else len(bikes) - (proc_num - 1) * line_per_proc
     shift = line_per_proc * proc_id
     out_bikes = defaultdict(list)
@@ -71,14 +46,13 @@ def output_func(line_per_proc, proc_id, bikes, fences):
         la = bikes.iloc[i + shift, 1]
         lo = bikes.iloc[i + shift, 2]
         point = (la, lo)
-        nearist, fence_pos = find_nearest(point, fences)
-
-        out_bikes['BIKE_ID'].append(bikes.iloc[i + shift, 0])
+        nearest, fence_pos = find_nearest(point, fences)
+        out_bikes['BIKE_ID'].append(bikes['BICYCLE_ID'][i + shift])
         out_bikes['BIKE_LATITUDE'].append(la)
         out_bikes['BIKE_LONGITUDE'].append(lo)
-        out_bikes['TIME'].append(bikes.iloc[i + shift, 3])
-        out_bikes['FENCE_LATITUDE'].append(nearist[0])
-        out_bikes['FENCE_LONGITUDE'].append(nearist[1])
+        out_bikes['TIME'].append(bikes['UPDATE_TIME'][i + shift])
+        out_bikes['FENCE_LATITUDE'].append(nearest[0])
+        out_bikes['FENCE_LONGITUDE'].append(nearest[1])
         out_bikes['FENCE_LATITUDE_0'].append(fence_pos[0])
         out_bikes['FENCE_LONGITUDE_0'].append(fence_pos[1])
         out_bikes['FENCE_LATITUDE_1'].append(fence_pos[2])
@@ -91,25 +65,27 @@ def output_func(line_per_proc, proc_id, bikes, fences):
             out_bikes['IS_INSIDE'].append(1)
         else:
             out_bikes['IS_INSIDE'].append(0)
-        print('row ' + str(i + shift) + ' finished')
+        if proc_id == 0:
+            print('row ' + str(i + shift) + ' finished on process ' + str(proc_id))
+    fname = './middleware/' + str(proc_id) + '.csv'
     out_bikes = pd.DataFrame(out_bikes)
-    return out_bikes
+    out_bikes.to_csv(fname, index = False)
 
 if __name__ == '__main__':
+    if not os.path.exists('./middleware'):
+        os.mkdir('./middleware')
+
     fences = pd.read_csv('./cleaned_data/fence_position.csv')
     bikes = pd.read_csv('./cleaned_data/required_bike_order.csv')
-    results = []
-    proc_num = 10
+
+    out_bikes = pd.DataFrame()
+    proc_num = 50
     line_per_proc = len(bikes)//(proc_num - 1) 
-    p = multiprocessing.Pool(proc_num)
+    pool = multiprocessing.Pool(processes = proc_num)
     for i in range(proc_num):
-        r = p.apply_async(output_func, args=(line_per_proc, i, bikes, fences))
-        results.append(r)
-    p.close()
-    p.join()
-    out_bikes = pd.concat(results, axis = 1)
-    print(out_bikes.sample(5))
-    out_bikes.to_csv('out_bikes.csv')
+        pool.apply_async(output_func, args = (line_per_proc,i, bikes, fences, proc_num))
+    pool.close()
+    pool.join()
     
 
 
--- tools/sql/find_between_7_9.sql
+++ tools/sql/find_between_7_9.sql
@@ -1,5 +1,10 @@
--- lock_status = 0: ����, 1: ����
+﻿-- lock_status = 0: 开锁, 1: 关锁
+--​任务一：为更好地掌握早高峰潮汐现象的变化规律与趋势，参赛者需基于主办方提供的数据进行数据分析和计算模型构建等工作，识别出工作日早高峰07:00-09:00潮汐现象最突出的40个区域
+--，列出各区域所包含的共享单车停车点位编号名称，并提供计算方法说明及计算模型，为下一步优化措施提供辅助支撑。 
+--任务二：参赛者根据任务一Top40区域计算结果进一步设计高峰期共享单车潮汐点优化方案，通过主动引导停车用户到邻近停车点位停车，进行削峰填谷，缓解潮汐点停车位（如地铁口）的拥堵问题。
+--允许参赛者自带训练数据，但需在参赛作品中说明所自带数据的来源及使用方式，并保证其合法合规。
+--（城市公共自行车从业者将发生在早晚高峰时段共享单车“借不到、还不进”的问题称之为“潮汐”现象。本题涉及的“潮汐现象”聚焦“还不进”的问题，识别出早高峰共享单车最淤积的40个区域）
 select top 1 * from (select convert(time, UPDATE_TIME) as dtime, * from gxdc_dd) as t1 where t1.LOCK_STATUS = 1 order by t1.dtime asc;
 select top 1 * from (select convert(time, UPDATE_TIME) as dtime, * from gxdc_dd) as t1 where t1. LOCK_STATUS = 0 order by t1.dtime desc;
-select BICYCLE_ID, LATITUDE, LONGITUDE, LOCK_STATUS, convert(smalldatetime, UPDATE_TIME) as UPDATE_TIME from (select *, datepart(hh, UPDATE_TIME) as happen_hour from gxdc_dd) as t1
-where ((t1.happen_hour < 9 and LOCK_STATUS = 0) or (t1.happen_hour > 7 and LOCK_STATUS = 1));
+select BICYCLE_ID, LATITUDE, LONGITUDE, convert(smalldatetime, UPDATE_TIME) as UPDATE_TIME from (select *, datepart(hh, UPDATE_TIME) as happen_hour from gxdc_dd) as t1
+where t1.happen_hour > 7 and t1.happen_hour < 9 and LOCK_STATUS = 1;
